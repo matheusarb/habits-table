b9e23bff014eb8aa4b2ab152e6c6a14c1ff224a5
Matheus <mat.araujoribeiro@gmail.com>
2023-01-18 12:41:39 -0300

    add mobile setup

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -5,11 +5,7 @@ interface HabitProps {
 }
 
 export function Habit(props: HabitProps) {
-  return (
-    <h1 className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center">
-      {props.completed}
-    </h1>
-  );
+  return <h1 className="habit">{props.completed}</h1>;
 }
 
 export default Habit;
