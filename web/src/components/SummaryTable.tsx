14de2f32d8c3382796880451f7571de4ab1d981d
Matheus <mat.araujoribeiro@gmail.com>
2023-01-20 18:35:33 -0300

    add amountOfDaysToFill logic

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -4,7 +4,9 @@ import { HabitDay } from "./HabitDay";
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 const summaryDates = generateDatesFromYearBeginning();
-console.log(summaryDates);
+
+const minimumSummaryDatesSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -26,6 +28,16 @@ export function SummaryTable() {
         {summaryDates.map((date) => {
           return <HabitDay key={date.toString()} />;
         })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
   );
