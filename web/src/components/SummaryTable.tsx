836928ef14cf77d15933e22eef8c962d08ceeb0b
Matheus <mat.araujoribeiro@gmail.com>
2023-01-20 18:07:54 -0300

    add generate-dates function and updating SummaryTable component

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,15 +1,30 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginnings";
+import { HabitDay } from "./HabitDay";
+
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+console.log(summaryDates);
 
 export function SummaryTable() {
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weekDays.map((weekDay) => {
+        {weekDays.map((weekDay, i) => {
           return (
-            <div className="text-zinc-400 text-xl h-10 w-10 dlex items-center justify-center font-bold">
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 dlex items-center justify-center font-bold"
+            >
               {weekDay}
             </div>
           );
+        })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
         })}
       </div>
     </div>
